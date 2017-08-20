e8f925b13be0ded758a4711bbf9106fe7429d8a2
Vladyslav Siriniok <siriniok@gmail.com>
2017-08-20 17:57:29 +0300

    Add some rules

--- tictactoe.js
+++ tictactoe.js
@@ -4,7 +4,7 @@ function nextItem(list) {
   if(this.index < list.length) this.index++;
   else this.index = 1;
 
-  return list[this.index -1];
+  return list[this.index - 1];
 }
 
 const board = [[null, null, null],
@@ -13,11 +13,26 @@ const board = [[null, null, null],
 
 const players = ['x', 'o'];
 
+let currentPlayer = nextItem(players);
+
 while(true) {
-  let currentPlayer = nextItem(players);
   console.log(board.map(row => row.map(e => e || ' ').join('|')).join('\n'));
 
-  const [row, col] = prompt('>> ').split(' ').map(e => Number(e));
+  const [row, col] = prompt('>> ').trim().split(' ').map(e => Number(e));
+
+  if((row < 0 || row >= board.length) ||
+     (col < 0 || col >= board[row].length)) {
+    console.log('Out of bounds, try another position');
+    continue;
+  }
+
+  if(board[row][col]) {
+    console.log('Cell occupied, try another position');
+    continue;
+  }
+
   board[row][col] = currentPlayer;
   console.log('\n');
+
+  currentPlayer = nextItem(players);
 }
