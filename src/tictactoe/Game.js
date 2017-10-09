4b4afb62c1d780c4d1440dbf808cb7b750488dc0
Vladyslav Siriniok <siriniok@gmail.com>
2017-10-09 14:42:14 +0300

    Break down the game cycle into several pieces

--- src/tictactoe/Game.js
+++ src/tictactoe/Game.js
@@ -7,77 +7,99 @@ module.exports = class Game {
                   [null, null, null]];
 
     this.players = ['x', 'o'];
+    this.currentPlayer = null;
+    this._index = 0;
   }
 
   play() {
-    let index = 0;
+    this._startNewTurn();
 
+    while(true) {
+      this._displayBoard();
+      const [row, col] = this._playerInput();
+
+      if(!this._isValidMove(row, col)) continue;
+
+      this.board[row][col] = this.currentPlayer;
+
+      if(this._isWinningMove(row, col)) {
+        console.log(`${this.currentPlayer} wins!`);
+        return;
+      }
+
+      if(this._isDraw()) {
+        console.log("It's a draw!");
+        return;
+      }
+
+      this._startNewTurn();
+    }
+  }
+
+  _startNewTurn() {
     function nextItem(list) {
-      if(index < list.length) index++;
-      else index = 1;
+      if(this._index < list.length) this._index++;
+      else this._index = 1;
 
-      return list[index - 1];
+      return list[this._index - 1];
     }
 
+    this.currentPlayer = nextItem.call(this, this.players);
+  }
+
+  _displayBoard() {
+    console.log(this.board.map(row => row.map(c => c || ' ').join('|')).join('\n'));
+  }
+
+  _playerInput() {
+    const coords = prompt('>> ').trim().split(' ').map(c => Number(c));
+    console.log();
+    return coords;
+  }
+
+  _isValidMove(row, col) {
+    if(!(Number.isInteger(row) && Number.isInteger(col)) ||
+       (row < 0 || row >= this.board.length) ||
+       (col < 0 || col >= this.board[row].length)) {
+      console.log('Coordinates are incorrect, try another position\n');
+      return false;
+    }
+
+    if(this.board[row][col]) {
+      console.log('Cell occupied, try another position\n');
+      return false;
+    }
+
+    return true;
+  }
+
+  _isWinningMove(row, col) {
     const leftDiagonal = [[0,0], [1,1], [2,2]];
     const rightDiagonal = [[2,0], [1,1], [0,2]];
 
-    let currentPlayer = nextItem(this.players);
+    let lines = [];
 
-    while(true) {
-      console.log(this.board.map(row => row.map(e => e || ' ').join('|')).join('\n'));
-
-      const [row, col] = prompt('>> ').trim().split(' ').map(e => Number(e));
-
-      if(!(Number.isInteger(row) && Number.isInteger(col)) ||
-        (row < 0 || row >= this.board.length) ||
-        (col < 0 || col >= this.board[row].length)) {
-        console.log('Coordinates are incorrect, try another position');
-        continue;
-      }
-
-      if(this.board[row][col]) {
-        console.log('Cell occupied, try another position');
-        continue;
-      }
-
-      this.board[row][col] = currentPlayer;
-      console.log('\n');
-
-      let lines = [];
-
-      [leftDiagonal, rightDiagonal].map((line) => {
-        line.map((cell) => {
-          if(cell[0] === row && cell[1] === col) lines.push(line);
-        });
+    [leftDiagonal, rightDiagonal].map((line) => {
+      line.map((cell) => {
+        if(cell[0] === row && cell[1] === col) lines.push(line);
       });
+    });
 
-      lines.push([0, 1, 2].map(c1 => [row, c1]));
-      lines.push([0, 1, 2].map(r1 => [r1, col]));
+    lines.push([0, 1, 2].map(c1 => [row, c1]));
+    lines.push([0, 1, 2].map(r1 => [r1, col]));
 
-      const win = lines.some((line) => {
-        return line.every((cell) => {
-          const [row, col] = cell;
+    return lines.some((line) => {
+      return line.every((cell) => {
+        const [row, col] = cell;
 
-          return this.board[row][col] === currentPlayer;
-        });
+        return this.board[row][col] === this.currentPlayer;
       });
+    });
+  }
 
-      if(win) {
-        console.log(`${currentPlayer} wins!`);
-        process.exit();
-      }
-
-      const draw = this.board.reduce((a, b) => a.concat(b), [])
-        .filter(cell => cell)
-        .length == 9;
-
-      if(draw) {
-        console.log("It's a draw!");
-        process.exit();
-      }
-
-      currentPlayer = nextItem(this.players);
-    }
+  _isDraw() {
+    return this.board.reduce((a, b) => a.concat(b), [])
+      .filter(cell => cell)
+      .length == 9;
   }
 };
